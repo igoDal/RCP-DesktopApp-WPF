1e140f02e2ebbb82842b9fcb198b81a6d0c13254
Igor Dalkiewicz <igortenn@gmail.com>
2022-12-13 17:46:45 +0100

    Add project files.

---
+++ RCP/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCP
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public virtual ICollection<Product>
+            Products
+        { get; private set; } =
+            new ObservableCollection<Product>();
+    }
+}
---
+++ RCP/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCP
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public DateOnly RCPDate { get; set; }
+        public TimeOnly RCPTime { get; set; }
+        public int RCPType { get; set; }
+        public int RCPKind { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
